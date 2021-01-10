bdcd84371b6f4ab0978d0adceadaf44d0bf65cda
Jon Ander <champi_thebest@hotmail.com>
2021-01-10 21:05:41 +0100

    Primer commit Lol_Stats_Bot en Python con Github

---
+++ Main.py
@@ -0,0 +1,22 @@
+import discord
+import json
+
+with open("elquetuquieras.json") as tempfile:
+    variables = json.load(tempfile)
+client = discord.Client()
+token = variables['token']
+prefix = variables['prefix']
+channel_bot = "habladución"
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has joined.')
+
+
+@client.event
+async def on_message(message):
+    if message.channel.name == channel_bot and message.content.startswith(prefix):
+        print(message.content)
+
+
+client.run(token)
