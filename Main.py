5b1780708d73ced1636d8d4dbd8fe7a72bb80a9f
Jon Ander <champi_thebest@hotmail.com>
2021-02-15 22:22:41 +0100

    añadir búsqueda de jugadores por ID, obtener rango, LP, victorias, derrotas y calcular winrate creando una función específica para ello

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ with open("keys.json") as tempfile:
 client = discord.Client()
 token = variables['token']
 prefix = variables['prefix']
+api_lol = variables['API_Lol']
 channel_bot = "habladución"
 
 @client.event
@@ -25,8 +26,27 @@ async def on_message(message):
     if message.content.startswith(Prefixes.INFO.value):
         invocador_name = data.replace(Prefixes.INFO.value, "", 1).strip()
         print(invocador_name)
-        print(requests.get('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/BrokenSwordParty?api_key=RGAPI-b93a0b44-0bbf-4434-aed6-803f92e2c5ad'))
+        print(requests.get(f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
+                           f'{invocador_name}?api_key={api_lol}'))
+        info_player = requests.get(f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
+                                   f'{invocador_name}?api_key={api_lol}')
+        summoner_id = info_player.json()['id']
+        account_id = info_player.json()['accountId']
+        puuid = info_player.json()['puuid']
+        print('Nivel: ' + str(info_player.json()['summonerLevel']))
+        requests.get(f'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/'
+                     f'{summoner_id}?api_key={api_lol}')
+        info_player2 = requests.get(f'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/'
+                                    f'{summoner_id}?api_key={api_lol}').json()
+        print(('SoloQ = '), info_player2[0]['tier'], info_player2[0]['rank'], info_player2[0]['leaguePoints'], 'Lp -',
+              info_player2[0]['wins'], 'W', info_player2[0]['losses'], 'L -',
+              winrate(info_player2[0]['wins'], info_player2[0]['losses']), '% Winrate')
+        print(('FlexQ = '), info_player2[1]['tier'], info_player2[1]['rank'], info_player2[1]['leaguePoints'], 'Lp -',
+              info_player2[1]['wins'], 'W', info_player2[1]['losses'], 'L -',
+              winrate(info_player2[1]['wins'], info_player2[1]['losses']), '% Winrate')
 
-
+def winrate(wins, losses):
+    calculator = round((wins*100)/(losses+wins))
+    return calculator
 
 client.run(token)
